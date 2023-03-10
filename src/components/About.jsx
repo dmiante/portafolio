902aeec7519f93bf6acd3c418985c4389fb966b0
damian <the.dmian@live.cl>
2023-03-10 20:42:03 -0300

    ADD resume about me and image profile

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,15 @@
 export default function About () {
   return (
     <>
-      <section id='about' className='mb-10 px-10'>
+      <section id='about' className='mb-10 px-10 py-24'>
         <div className='flex justify-center mb-14'>
           <h2 className='text-center text-2xl font-bold text-gray-700'>SOBRE MÍ</h2>
         </div>
-        <div className='grid grid-cols-1 items-start h-auto gap-5'>
-          <p className='text-gray-600 text-center font-semibold'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor vero corrupti consequatur esse nemo mollitia neque rerum est. Repellat similique ab minima quidem quis voluptatem ratione libero dolorem accusantium soluta.</p>
-          <img src='../src/assets/projects/card-top.jpg' alt='profile' />
+        <div className='grid grid-cols-1 items-start h-auto gap-10'>
+          <p className='text-gray-600 text-center font-semibold text-base'>
+            Soy Damián, ingeniero en informática apasionado por el desarrollo de software. Poseo conocimientos sólidos en programación, diseño y desarrollo web, así como en diversas herramientas y frameworks como React y NodeJS. Me considero una persona proactiva, comprometida y dispuesta a aprender. Actualmente resido en Santiago de Chile y busco una oportunidad para aplicar mis habilidades y conocimientos en un entorno de trabajo profesional.
+          </p>
+          <img src='../src/assets/profile_BW.jpeg' alt='profile' loading='lazy' className='rounded-3xl border-logo border-4' />
         </div>
       </section>
     </>
