73740282901141c0b14e785a63f574c6610c205a
damian <the.dmian@live.cl>
2023-03-17 18:54:18 -0300

    UPDATE styles hover and change size of the icons

--- src/data/Socials.jsx
+++ src/data/Socials.jsx
@@ -2,15 +2,15 @@ import { SiGithub, SiGmail, SiLinkedin } from 'react-icons/si'
 
 export const socialLinks = [
   {
-    url: 'https://github.com/dmi4n',
-    icon: <SiGithub name='Github' size={20} />
+    url: 'https://github.com/dmiante',
+    icon: <SiGithub name='Github' size={40} />
   },
   {
     url: 'https://www.linkedin.com/in/damian-soto/',
-    icon: <SiLinkedin name='Linkedin' size={20} />
+    icon: <SiLinkedin name='Linkedin' size={40} />
   },
   {
     url: 'mailto:soto.damian02@gmail.com',
-    icon: <SiGmail name='Gmail' size={20} />
+    icon: <SiGmail name='Gmail' size={40} />
   }
 ]
