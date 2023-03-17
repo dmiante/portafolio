d024917e7986a2432d912721a83340aff2365503
damian <the.dmian@live.cl>
2023-03-17 18:47:08 -0300

    ADD tailwind config fonts family and background

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,6 +12,12 @@ module.exports = {
         headline: '#0d0d0d',
         paragraph: '#2a2a2a',
         logo: '#ff4057'
+      },
+      backgroundImage: {
+        'pattern-primary': "url('/src/assets/hyperspace.svg')"
+      },
+      fontFamily: {
+        sans: ['Rubik', 'sans-serif']
       }
     }
   },
