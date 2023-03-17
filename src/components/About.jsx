930099615b70bdf5b3a938a63225b25e2280128e
damian <the.dmian@live.cl>
2023-03-17 18:50:24 -0300

    UPDATE paragraph about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,15 @@
 export default function About () {
   return (
     <>
-      <section id='about' className='mb-10 px-10 pt-24'>
+      <section id='about' className='mb-10 py-24 px-10'>
         <div className='flex justify-center mb-14'>
-          <h2 className='text-center text-2xl font-bold text-gray-700'>SOBRE MÍ</h2>
+          <h2 className='text-center text-4xl md:text-5xl font-bold text-gray-700 mb-5'>SOBRE MÍ</h2>
         </div>
-        <div className='grid grid-cols-1 items-start h-auto gap-10'>
-          <p className='text-gray-600 text-center font-semibold text-base'>
-            Soy Damián, ingeniero en informática apasionado por el desarrollo de software. Poseo conocimientos sólidos en programación, diseño y desarrollo web, así como en diversas herramientas y frameworks como React y NodeJS. Me considero una persona proactiva, comprometida y dispuesta a aprender. Actualmente resido en Santiago de Chile y busco una oportunidad para aplicar mis habilidades y conocimientos en un entorno de trabajo profesional.
+        <div className='grid grid-cols-1 items-center h-auto gap-10 lg:my-0 md:grid-cols-2 place-items-center'>
+          <p className='text-gray-600 text-center font-semibold text-base md:text-xl lg:text-2xl'>
+            Soy <span className='italic underline underline-offset-2 decoration-2 decoration-logo hover:bg-logo hover:text-white hover:p-2 hover:rounded-xl'>Damián Soto</span>, un desarrollador de software de Santiago titulado de ingeniero en informática. Me considero proactivo, comprometido y apasionado por la tecnología 🖥️ y los videojuegos 🕹️. Actualmente aprendo programación con <span className='text-yellow-500'>JavaScript</span> y <span className='text-sky-500'>React</span>
           </p>
-          <img src='../src/assets/profile_BW.jpeg' alt='profile' loading='lazy' className='rounded-3xl border-logo border-4' />
+          <img src='../src/assets/profile_BW.jpeg' alt='profile' loading='lazy' className='rounded-3xl border-logo border-4' width={350} />
         </div>
       </section>
     </>
