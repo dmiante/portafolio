cea865b050f1a0cf9489a19b7827d0f7fb167885
damian <the.dmian@live.cl>
2023-04-14 19:07:13 -0400

    ADD hooks for dark mode

---
+++ src/hooks/useTheme.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react'
+
+export function useTheme () {
+  const [theme, setTheme] = useState('dark')
+
+  useEffect(() => {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      setTheme('dark')
+    } else {
+      setTheme('light')
+    }
+  }, [])
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+  }, [theme])
+
+  function handleClick () {
+    setTheme(theme === 'dark' ? 'light' : 'dark')
+    // console.log(theme)
+  }
+
+  return {
+    theme,
+    setTheme,
+    handleClick
+  }
+}
