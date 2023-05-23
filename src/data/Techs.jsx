3c36424484d0fd87380740f6545df9d4dcd02205
damian <the.dmian@live.cl>
2023-05-22 20:50:14 -0400

    ADD alt to img

--- src/data/Techs.jsx
+++ src/data/Techs.jsx
@@ -11,38 +11,38 @@ import Vscode from '../assets/skill/vscode.svg'
 export const technologies = [
   {
     name: 'HTML5',
-    icon: <img width={50} height={50} src={Html} />
+    icon: <img width={50} height={50} src={Html} alt='html' />
   },
   {
     name: 'CSS3',
-    icon: <img width={50} height={50} src={Css} />
+    icon: <img width={50} height={50} src={Css} alt='css3' />
   },
   {
     name: 'JavaScript',
-    icon: <img width={50} height={50} src={Javascript} />
+    icon: <img width={50} height={50} src={Javascript} alt='javascript' />
   },
   {
     name: 'NodeJS',
-    icon: <img width={50} height={50} src={Nodejs} />
+    icon: <img width={50} height={50} src={Nodejs} alt='nodejs' />
   },
   {
     name: 'ReactJS',
-    icon: <img width={50} height={50} src={React} />
+    icon: <img width={50} height={50} src={React} alt='reactjs' />
   },
   {
     name: 'TailwindCSS',
-    icon: <img width={50} height={50} src={Tailwindcss} />
+    icon: <img width={50} height={50} src={Tailwindcss} alt='tailwindcss' />
   },
   {
     name: 'Git',
-    icon: <img width={50} height={50} src={Git} />
+    icon: <img width={50} height={50} src={Git} alt='git' />
   },
   {
     name: 'MySql',
-    icon: <img width={50} height={50} src={Mysql} />
+    icon: <img width={50} height={50} src={Mysql} alt='mysql' />
   },
   {
     name: 'VSCODE',
-    icon: <img width={50} height={50} src={Vscode} />
+    icon: <img width={50} height={50} src={Vscode} alt='vscode' />
   }
 ]
