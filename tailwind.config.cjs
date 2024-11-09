996812b6519ee3bc15b175b732f73916d3077109
dmiante <dmiantedev@gmail.com>
2024-11-09 01:11:16 -0300

    update: icons

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,11 +8,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#eff0f3',
-        secondary: '#ff8e3c',
-        headline: '#0d0d0d',
-        paragraph: '#2a2a2a',
-        logo: '#ff4057'
+        highlight: '#ff4057'
       },
       backgroundImage: {
         'pattern-dark': "url('/src/assets/hyperspace_dark.svg')",
