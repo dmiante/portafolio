231907d670e9c4107bde1e38eec91bb07b96ec33
dmiante <dmiantedev@gmail.com>
2024-11-22 23:10:03 -0300

    fix projects data

--- src/data/Projects.jsx
+++ src/data/Projects.jsx
@@ -1,9 +1,20 @@
-import Climapp from '../assets/projects/climapp.webp'
-import Giffy from '../assets/projects/giffy.webp'
-import Portafolio from '../assets/projects/portafolio.webp'
-import PokeChang from '../assets/projects/pokechang.webp'
-import Removebg from '../assets/projects/removebg.webp'
-import Shrtu from '../assets/projects/shrtu.webp'
+import Climapp from '../assets/projects/climapp.png'
+// import Giffy from '../assets/projects/giffy.webp'
+// import Portafolio from '../assets/projects/portafolio.webp'
+import PokeChang from '../assets/projects/pokechang.png'
+import Removebg from '../assets/projects/removebg.png'
+import Shrtu from '../assets/projects/shrtu.png'
+
+import Css from '../assets/skill/css.svg'
+import Javascript from '../assets/skill/javascript.svg'
+import React from '../assets/skill/react.svg'
+import Tailwindcss from '../assets/skill/tailwindcss.svg'
+import Svelte from '../assets/skill/svelte.svg'
+import TypeScript from '../assets/skill/typescript.svg'
+import Nextjs from '../assets/skill/nextjs.svg'
+import Prisma from '../assets/skill/prisma.svg'
+import Supabase from '../assets/skill/supabase.svg'
+import Cloudinary from '../assets/skill/cloudinary.svg'
 
 export const PROJECTS_DATA = [
   {
@@ -12,26 +23,29 @@ export const PROJECTS_DATA = [
     gh: 'https://github.com/dmiante/climapp',
     url: 'https://climapp-dmiante.vercel.app/',
     image: Climapp,
-    tags: ['React', 'JavaScript', 'CSSModules'],
-    show: true
+    tags: [{ tech: 'ReactJS', icon: React }, { tech: 'JavaScript', icon: Javascript }, { tech: 'CSS', icon: Css }],
+    show: true,
+    priority: 3
   },
   {
     title: 'Giffy',
     description: 'Clon de Giphy.com hecho con React.',
     gh: 'https://github.com/dmiante/giffy',
     url: 'https://giffy-dmiante.vercel.app/',
-    image: Giffy,
-    tags: ['React', 'JavaScript', 'CSS'],
-    show: false
+    image: null,
+    tags: [{ tech: 'ReactJS', icon: React }, { tech: 'JavaScript', icon: Javascript }, { tech: 'CSS', icon: Css }],
+    show: false,
+    priority: 2
   },
   {
     title: 'Portafolio',
     description: 'Portafolio web donde muestro mis proyectos y tecnologías utilizadas.',
     gh: 'https://github.com/dmiante/portafolio',
     url: 'https://damiansoto.dev/',
-    image: Portafolio,
-    tags: ['React', 'JavaScript', 'TailwindCSS'],
-    show: false
+    image: null,
+    tags: [{ tech: 'ReactJS', icon: React }, { tech: 'JavaScript', icon: Javascript }, { tech: 'TailwindCSS', icon: Tailwindcss }],
+    show: false,
+    priority: 1
   },
   {
     title: 'PokeChang',
@@ -39,8 +53,9 @@ export const PROJECTS_DATA = [
     gh: 'https://github.com/dmiante/poke-chang',
     url: 'https://poke-chang.vercel.app',
     image: PokeChang,
-    tags: ['React', 'JavaScript', 'TailwindCSS', 'PokeAPI'],
-    show: true
+    tags: [{ tech: 'ReactJS', icon: React }, { tech: 'JavaScript', icon: Javascript }, { tech: 'TailwindCSS', icon: Tailwindcss }, { tech: 'PokeAPI', icon: null }],
+    show: true,
+    priority: 3
   },
   {
     title: 'RemoveBG',
@@ -48,8 +63,9 @@ export const PROJECTS_DATA = [
     gh: 'https://github.com/dmiante/removebg',
     url: 'https://removebackground.vercel.app',
     image: Removebg,
-    tags: ['Svelte', 'TypeScript', 'TailwindCSS', 'Cloudinary'],
-    show: true
+    tags: [{ tech: 'Svelte', icon: Svelte }, { tech: 'TypeScript', icon: TypeScript }, { tech: 'TailwindCSS', icon: Tailwindcss }, { tech: 'Cloudinary', icon: Cloudinary }],
+    show: true,
+    priority: 4
   },
   {
     title: 'shrtu',
@@ -57,7 +73,8 @@ export const PROJECTS_DATA = [
     gh: 'https://github.com/dmiante/short-url',
     url: 'https://shrtu.vercel.app',
     image: Shrtu,
-    tags: ['NextJS', 'TypeScript', 'TailwindCSS', 'Prisma', 'Supabase'],
-    show: true
+    tags: [{ tech: 'NextJS', icon: Nextjs }, { tech: 'TypeScript', icon: TypeScript }, { tech: 'TailwindCSS', icon: Tailwindcss }, { tech: 'Prisma', icon: Prisma }, { tech: 'Supabase', icon: Supabase }],
+    show: true,
+    priority: 5
   }
 ]
